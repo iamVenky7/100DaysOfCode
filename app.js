c223edd0d88d33db368dd5b95596e507e3f71330
Venky <venkateshpamidi7@gmail.com>
2022-06-21 09:58:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// document.body.style.backgroundColor = "yellow";
+// document.body.style.color = "red";
+// document.getElementById("btn").style.color = "black";
+
+// const h1 = document.getElementById("title");
+
+// h1.style.color = "red";
+
+// const btn = document.getElementById("btn");
+
+// btn.style.backgroundColor = "blue";
+// btn.style.color = "white";
+
+// document.getElementById("btn").style.backgroundColor = "blue";
+
+// document.getElementById("btn").style.color = "white";
+
+//const headings = document.getElementsByTagName("h2");
+
+//headings[0].style.color = "red";
+
+// console.log(headings.length);
+
+//const items = document.getElementsByTagName("li");
+
+//const betterItems = [...items];
+
+// betterItems.forEach(function (item) {
+//   console.log(item);
+// });
+
+/* const btn = document.querySelector(".btn");
+const heading = document.querySelector("h2");
+btn.addEventListener("click", function () {
+  heading.classList.add("red");
+});
+ */
