f3b623bac3482b84687fac2b730d480578d64a9d
Venky <venkateshpamidi7@gmail.com>
2022-07-10 13:09:01 +0530

    JS intro❤️

---
+++ Day29,30/script.js
@@ -0,0 +1,31 @@
+//Understanding Values
+
+/* 
+"Hi, I'm Venky!" --> Text(String)
+22 --> Number
+There are more available values: Booleans, objects, arrays,...
+
+**Understaning Variables**
+
+Variables are "labeled data containers"
+age --- Label("Name of the Variable") */
+
+let greetingText = "Hi, I'm venky!";
+let myAge = 22;
+// alert(greetingText);
+// alert(greetingText);
+greetingText = "Hi, I am really venky";
+/* alert(greetingText);
+alert(myAge);
+alert(myAge); */
+
+//ARRAYS--- used for storing related data
+
+let age = 22;
+let name = "Venky";
+let hobbies = ["Sports", "Cooking", "Reading"];
+//alert(hobbies[0]); //to get particular index value
+
+//OBJECTS
+
+let job = { title: "Developer", place: "Banglore", salary: 30000 };
