d5f962d309b52be1c3fa06c81b7a2a124a0b1224
Venky <venkateshpamidi7@gmail.com>
2022-07-14 22:50:33 +0530

    Working with DOM

---
+++ Day33,34/script.js
@@ -0,0 +1,15 @@
+/* "Window" & "document"  
+----------------------
+-> Built-in Variables & Functions
+Global "window" Object - Holds information and functionality related to the active browser window/tab */
+//e.g. current location, utility function like alert()
+/* 
+The "document" Object (window.document)-- Holds information and functionality to the loaded website content
+e.g.utility functions to access HTML elements */
+
+//console.dir(document);
+/* 
+
+alert();
+window.alert("Hey!"); */
+document.objext.
