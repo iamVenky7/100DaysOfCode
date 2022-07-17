1a775c53e8b215191a92a35bbb408f2cb248f21f
Venky <venkateshpamidi7@gmail.com>
2022-07-17 15:32:45 +0530

    Working with DOM

---
+++ Day33,34/exercise.js
@@ -0,0 +1,30 @@
+// 1. Select the <h1> element by "drilling into DOM" and save it in a variable with a name of your choice
+
+let h1Element = document.body.firstElementChild;
+h1Element = document.body.children[0];
+
+console.dir(h1Element);
+
+// 2. Use the variable from (1) and get access to the 'parent' element of the stored <h1> element (i.e. to the <body> element)
+//    element of the stored <h1> element (i.e. to the <body> element )
+
+console.dir(h1Element.parentElement);
+//    BONUS: Try using the variable from(1) to get access to the
+//    sibling element(i.e. the <p> element next to the <h1> element)
+
+console.dir(h1Element.nextElementSibling);
+
+// 3. Select the <h1> element with getElementById and store in the same or a new variable (up to you)
+
+h1Element = document.getElementById("head");
+console.dir(h1Element);
+
+// 4. Select the second <p> element with querySelector (you might need to add something in the HTML ConvolverNode, e,g. a class)
+//       and store it in new variable with a name of your choice
+
+let paragraph2 = document.querySelector(".para2");
+console.dir(paragraph2);
+
+// 5. BONUS TASK: Try changing the text content of the <p> element you selected in (4) and set it to any other text of your choice
+
+paragraph2.textContent = "this was changes by Venky";
--- Day33,34/script.js
+++ Day33,34/script.js
@@ -7,9 +7,14 @@ Global "window" Object - Holds information and functionality related to the acti
 The "document" Object (window.document)-- Holds information and functionality to the loaded website content
 e.g.utility functions to access HTML elements */
 
-//console.dir(document);
+//document.body.children[1].children[0].href = "https://www.google.com";
 /* 
-
+console.dir(document);
 alert();
 window.alert("Hey!"); */
-document.objext.
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
+
+anchorElement = document.querySelector("p a"); //p a {color:red;}  same as css.  to select all querSelectorAll
+anchorElement.href = "https://facebook.com";
